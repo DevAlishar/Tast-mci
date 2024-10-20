ff9758406f1f1a6143eb0500f0cf6fa5f1b70e63
AliShariatiFar <140342339+DevAlishar@users.noreply.github.com>
2024-10-20 19:33:30 +0330

    Update extract

--- modul/extract.py
+++ modul/extract.py
@@ -3,9 +3,16 @@ import json
 
 async def extract(html, xpath):
         dom = etree.HTML(html)
-        price = dom.xpath(xpath)
-        if not price or not isinstance(price[0], str) and not price[0].text:
-            print("No price extracted or incorrect XPath.")
-            return
-        extracted_price = price[0].text if hasattr(price[0], 'text') else price[0]
-        return extracted_price.strip() if extracted_price else None
+        nodes = dom.xpath(xpath)
+        result = []
+        for node in nodes:
+            if hasattr(node, 'text'):
+                result.append(node.text)
+            else:
+                if isinstance(node, str):
+                    result.append(node)
+                elif isinstance(node, bytes):
+                    result.append(node.decode())
+                else:
+                    result.append(str(node))
+        return result
