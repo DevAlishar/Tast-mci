893557c002013bc285c69b6e26b265e4c87fe314
AliShariatiFar <140342339+DevAlishar@users.noreply.github.com>
2024-10-20 08:54:51 +0330

    Yoooo

--- modul/datacore.py
+++ modul/datacore.py
@@ -1,11 +1,16 @@
 import re
 import pandas as pd
 from urllib.parse import urlparse
+import json
+from exceptions import ScrapException
+
+class NoXPathError(ScrapException): ...
 
 class BaseWebRepo:
     async def GetXPaths(self, url):
         raise NotImplementedError()
-
+    def close(self): 
+        pass
 class PandasWebRepo(BaseWebRepo):
     def __init__(self, dataframe:pd.DataFrame):
         self.df = dataframe
@@ -28,6 +33,20 @@ class PandasWebRepo(BaseWebRepo):
                 break
         return result
 
+
+class JsonWebRepo(BaseWebRepo):
+    def __init__(self, json_file):
+        self.json = json.loads(open(json_file, 'r+').read())
+    async def GetXPaths(self, url):
+        parsed_url = urlparse(url)
+        domain = parsed_url.netloc
+        xpaths=self.json.get(domain.__str__().lower(), None)
+        result={}
+        if xpaths is None:
+            raise NoXPathError("XPath not exists")
+        for i, xpath in enumerate(xpaths):
+            result[str(i)]=xpath
+        return result
 class ExcelWebRepo(PandasWebRepo):
     def __init__(self, ExcelFileRepo):
         df = pd.read_excel(ExcelFileRepo)
