ad8f4dcf618aab7585e3fc8a975a8911e19dc6f1
AliShariatiFar <140342339+DevAlishar@users.noreply.github.com>
2024-10-20 19:33:59 +0330

    Update fetch

--- modul/fetch.py
+++ modul/fetch.py
@@ -1,4 +1,9 @@
 from aiohttp import ClientSession
+from aiohttp.client_exceptions import ClientConnectorError, ConnectionTimeoutError, ClientPayloadError
+from asgiref.sync import async_to_sync
+from exceptions import ScrapException
+
+class ConnectionError(ScrapException): ...
 
 class fetcher:
     def __init__(self):
@@ -8,9 +13,13 @@ class fetcher:
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
         }
-        response = await self.session.get(url, headers=headers)
+        try:
+            response = await self.session.get(url, headers=headers)
+        except (ClientConnectorError, ConnectionTimeoutError, ClientPayloadError):
+            raise ConnectionError("Connection Failed")
         html = await response.text()
+        response.close()
         return html
 
-    def close(self):
-        self.session.close()
+    async def close(self):
+        await self.session.close()
