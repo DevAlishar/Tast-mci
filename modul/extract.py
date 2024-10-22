a3161f3dfcd7596d51e0656cf848f1cb0c6c2d37
AliShariatiFar <140342339+DevAlishar@users.noreply.github.com>
2024-10-22 13:14:48 +0330

    Update

--- modul/extract.py
+++ modul/extract.py
@@ -1,13 +1,19 @@
 from lxml import etree
 import json
 
+def extractNode(node):
+    base = node.text
+    for ch in node.getchildren():
+        base += extractNode(ch)
+    return base
+
 async def extract(html, xpath):
         dom = etree.HTML(html)
         nodes = dom.xpath(xpath)
         result = []
         for node in nodes:
             if hasattr(node, 'text'):
-                result.append(node.text)
+                result.append(extractNode(node))
             else:
                 if isinstance(node, str):
                     result.append(node)
